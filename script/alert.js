f578d7867bfd1d384e14991bd429adf420f8b1fb
wilco <wilcologger@gmail.com>
2022-08-17 14:43:18 +0200

    first commit

---
+++ script/alert.js
@@ -0,0 +1,9 @@
+/* 
+    step 1: 
+    Create a page that shows a message “I’m JavaScript!”.
+
+    step 2:
+    Take the solution of the previous task Show an alert. Modify it by extracting 
+    the script content into an external file alert.js, residing in the same folder.
+*/ 
+alert("I\'m Javascript!");
---
+++ script/variable.js
@@ -0,0 +1,26 @@
+/* 
+    step 1:
+    1. Declare two variables: admin and name.
+    2. Assign the value "John" to name.
+    3. Copy the value from name to admin.
+    4. Show the value of admin using alert (must output “John”).
+*/
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+admin = name;
+
+alert(admin); // "John"
+
+/*
+    step 2:
+    1. Create a variable with the name of our planet. How would you name such a variable?
+    2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+*/
+
+let planetName = 'Earth';
+// let ourPlanetName = "Earth";
+
+let currentVisitor;
+// let currentUserName = "John";
