6c8d8570815ddbb0eb1d3e96d70c858e5085ff49
wilco <wilcologger@gmail.com>
2022-08-29 13:42:46 +0200

    switch

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 /* 
-   step 1: maak simple een button die een class toggled
-   stap 2: 
+   step 1: maak meerder divs
+   stap 2: eventlister(click) toevoegen 
+   stap 3: queryselectorall met een index om een class te toggle
 */
 
-// onclick = (button) => {
-//     // console.log('i\'m clicked');
-//     if (button == true) {
-//         console.log('true');
-//     } else {
-//         toggleBody.toggle('show');
-//     }
-// };
-
 function toggleButton() {
-  let toggleElement = document.getElementById("toggleBody");
-  toggleElement.classList.toggle("show");
+    let toggleElement = document.getElementById("toggleBody");
+    toggleElement.classList.toggle("show");
 }
