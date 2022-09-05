1144196c75c9785710a6bfce4d4b97ff36b1e2dc
wilco <wilcologger@gmail.com>
2022-09-05 09:47:43 +0200

    FAQ component

--- script.js
+++ script.js
@@ -8,3 +8,18 @@
 //     let toggleElement = document.getElementById("toggleBody");
 //     toggleElement.classList.toggle("show");
 // }
+
+// Node list
+// OF
+// array
+
+// array methods 
+
+const buttons = document.querySelectorAll('.accordion-button');
+let bodys = document.querySelectorAll('.accordion-collapse');
+
+for (let i = 0; i < buttons.length; i++) {
+   buttons[i].addEventListener('click', (e) => { 
+      bodys[i].classList.toggle('show');
+   });   
+}
